0f5c092edaea9f53acfbc4f81dca4cf08869f969
0975062777 <redsand.trananh@gmail.com>
2020-10-06 15:06:36 +0700

    add new file

---
+++ Tạo layout cho trang web/header/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        nav {
+            float: left;
+            max-width: 160px;
+            margin: 0;
+            padding: 1em;
+        }
+
+        nav ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        nav ul a {
+            text-decoration: none;
+        }
+
+        article {
+            margin-left: 170px;
+            border-left: 1px solid gray;
+            padding: 1em;
+            overflow: hidden;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+    echo "<h1>City Gallery</h1>";
+    ?>
---
+++ Tạo layout cho trang web/index.php
@@ -0,0 +1,18 @@
+<?php require "header/header.php" ?>
+
+<ul>
+    <li><a href="#">London</a></li>
+    <li><a href="#">Paris</a></li>
+    <li><a href="#">Tokyo</a></li>
+</ul>
+
+<h1>London</h1>
+<p>London is the capital city of England. It is the most populous city in the
+    United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+<p>Standing on the River Thames, London has been a major settlement for two
+    millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+
+<?php require "foorter.php" ?>
+</body>
+
+</html>
